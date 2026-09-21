376befd8376f006f4fa257cbbe6539a5f76cc7e6
bernardoamorim6 <bernardoamorim123@gmail.com>
2026-09-21 21:45:18 +0100

    create cmake toolchain file

---
+++ cmake/arm-none-eabi.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME  Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_C_FLAGS_INIT "-mcpu=cortex-m0plus -mthumb")
+set(CMAKE_CXX_FLAGS_INIT "-mcpu=cortex-m0plus -mthumb")
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
